89f4a1f2ca7e1b2cc78241edbf2d9787c4e40353
herculesoliveira40 <herculesoliveira40@gmail.com>
2023-02-24 16:02:50 -0300

    meta description

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Site comments users">
     <link rel="icon" href="#.png" sizes="32x32" type="image/png">
     <title>@yield('title') :D</title>
 
